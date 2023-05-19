305b76540d1b3f95bb1c5f7563908f89eab00268
jilink <jimmyfac@gmail.com>
2023-05-19 10:25:09 +0200

    pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+function Pagination({list, setList, resultsPerPage = 10}) {
+
+  const [page, setPage] = useState(0)
+
+  useEffect(() => {
+    setList(list.slice(page * resultsPerPage, page * resultsPerPage + resultsPerPage))
+  }, [page, resultsPerPage, list , setList])
+  
+
+  function handleNextPage() {
+    setPage(page + 1)
+  }
+
+  function handlePreviousPage() {
+    if (page === 0) {
+      return
+    }
+    setPage(page - 1)
+  }
+  return (
+    <>
+      <button onClick={handlePreviousPage}>Page précédente</button>
+      <h1>{page + 1}</h1>
+      <button onClick={handleNextPage}>Page suivante</button>
+      </>
+  )
+}
+
+export default Pagination
--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,28 +1,37 @@
-import React, {useState, useEffect}  from "react";
-import axios from "axios"
-import Pokemon from '../Pokemon';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Pokemon from "../Pokemon";
+import Pagination from "../Pagination";
 
 function PokemonList({ searchTerm = "" }) {
-
-
-  const [pokemonList, setPokemonList] = useState([])
+  const [pokemonList, setPokemonList] = useState([]);
+  const [paginatedList, setPaginatedList] = useState([]);
 
   async function getData() {
-    const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100")
-    setPokemonList(res.data.results)
+    const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100");
+    setPokemonList(res.data.results);
   }
 
   useEffect(() => {
-    getData()
-    console.log(process.env.REACT_APP_API_KEY)
-  }, [])
+    getData();
+    console.log(process.env.REACT_APP_API_KEY);
+  }, []);
 
   return (
-    <ul>
-      {pokemonList.filter((pokemon) => pokemon.name.includes(searchTerm)).map((pokemon) => (
-        <Pokemon pokemon={pokemon} key={pokemon.name}/>
-      ))}
-    </ul>
+    <>
+      <Pagination
+        list={pokemonList.filter((pokemon) =>
+          pokemon.name.includes(searchTerm)
+        )}
+        setList={setPaginatedList}
+        // resultsPerPage={2}
+      />
+      <ul>
+        {paginatedList.map((pokemon) => (
+        <Pokemon pokemon={pokemon} key={pokemon.name} />
+        ))}
+      </ul>
+    </>
   );
 }
 
